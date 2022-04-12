18a9d88d672db99a95fff5e2140d3287de6a1f85
Athéna Richard <richard.athena@gmail.com>
2022-04-12 17:51:59 +0200

    Ajout de sécurité

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,24 @@ const express = require ("express");
 // Import de Mongoose
 const mongoose = require ("mongoose");
 
+// Import de helmet pour sécuriser les requêtes http d'express
+const helmet = require("helmet");
+
+// Import de mongo-sanatize pour nettoyer les données des utilisateurs (empêche l'injection)
+const mongoSanitize = require ("express-mongo-sanitize"); 
+
 // Import de path pour le chemin des images
 const path = require ("path");
 
 // Import des router
 const sauceRoutes = require ("./routes/sauce");
 const utilisateurRoutes = require ("./routes/utilisateur");
+
+// Import de cors pour sécuriser l'accès à l'API, réservé ici à localhost:4200
+const cors = require ("cors");
+let corsOptions = {
+    origin: 'http://localhost:4200'
+}
 
 // Connexion de l'API à la base de données MongoDB
 mongoose.connect(`mongodb+srv://${process.env.Mongo_Utilisateur}:${process.env.Mongo_Motdepasse}@${process.env.Mongo_Hote}/${process.env.Mongo_Nom}?retryWrites=true&w=majority`, {
@@ -36,6 +48,16 @@ application.use((requete, reponse, next) => {
     reponse.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+// Utilisation de cors
+application.use(cors(corsOptions));
+
+// Utilisation de helmet pour sécuriser les en-têtes, en cross-origins pour permettre à l'utilisateur d'accéder à des ressoucres d'un autre serveur
+application.use (helmet.crossOriginResourcePolicy ({ policy : "cross-origin" }));
+
+// Par défaut, les caractères $ et . sont complètement supprimés des entrées fournies par l'utilisateur aux endroits suivants : req.body, req.params, req.headers, req.query
+// Pour supprimer les données en utilisant ces valeurs par défaut
+application.use(mongoSanitize());
 
 // Utilisation des images dans le dossier statique
 application.use("/images", express.static(path.join(__dirname, "images")));
--- backend/routes/utilisateur.js
+++ backend/routes/utilisateur.js
@@ -4,12 +4,21 @@ const express = require ("express");
 // Création d'un router via Express
 const router = express.Router();
 
-// Import du contrôleur sauce
+// Import du middleware email pour contrôler l'email
+const emailControle = require ("../middleware/email");
+
+// Import du middleware motdepasse pour contrôler le mot de passe
+const motdepasseControle = require ("../middleware/motdepasse");
+
+// Import du middleware connexion pour limiter les tentatives de connexion
+const connexionControle = require ("../middleware/connexion");
+
+// Import du contrôleur utilisateur
 const utilisateurControle = require ("../controllers/utilisateur");
 
 // Utilisation de l'application via l'implémenter du CRUD (creation, read, update, delete)
-router.post('/signup', utilisateurControle.creationCompte);
-router.post('/login', utilisateurControle.connexionCompte);
+router.post('/signup', emailControle, motdepasseControle, utilisateurControle.creationCompte);
+router.post('/login', connexionControle, utilisateurControle.connexionCompte);
 
 // Export du router
 module.exports = router;
